449719107748da49a043ab7292c0e7fd4563538b
Darren Mulholland <dmulholl@tcd.ie>
2020-11-08 22:17:33 +0000

    Update packaging machinery

--- setup.py
+++ setup.py
@@ -3,15 +3,26 @@
 Argslib
 =======
 
-A minimalist library for parsing command line arguments.
+A library for parsing command line arguments.
 
 """
 
+import os
+import re
+import io
+
 from setuptools import setup
+
+
+metapath = os.path.join(os.path.dirname(__file__), 'argslib.py')
+with io.open(metapath, encoding='utf-8') as metafile:
+    regex = r'''^__([a-z]+)__ = ["'](.*)["']'''
+    meta = dict(re.findall(regex, metafile.read(), flags=re.MULTILINE))
+
 
 setup(
     name = 'argslib',
-    version = '2.0.0',
+    version = meta['version'],
     py_modules = ['argslib'],
     author = 'Darren Mulholland',
     url = 'https://github.com/dmulholl/argslib',
